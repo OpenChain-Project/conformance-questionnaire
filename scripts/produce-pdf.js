22d9659f215765b9d12c1f8d7c2e7df025f3ac62
Gary O'Neall <gary@sourceauditor.com>
2018-08-30 10:28:47 -0700

    Add preamble text to the pdf file
    Signed-off-by: Gary O'Neall <gary@sourceauditor.com>

--- scripts/produce-pdf.js
+++ scripts/produce-pdf.js
@@ -11,12 +11,27 @@ var glob = require('glob');
 var fs = require('fs');
 var path = require('path');
 
-var outputDir = "../pdfs/";
-var srcDir = "../";
+var outputDir = "./pdfs/";
+var srcDir = "./";
 
 var font = "Times-Roman";
 var sectionFontSize = 16;
 var questionFontSize = 10;
+
+var preambleText = ['The OpenChain Project builds trust in open source by making open source license compliance '
+							+ 'simpler and more consistent. The OpenChain Specification defines a core set of requirements every quality '
+							+ 'compliance program must satisfy. The OpenChain Curriculum provides the educational '
+							+ 'foundation for open source processes and solutions, whilst meeting a key requirement '
+							+ 'of the OpenChain Specification. OpenChain Conformance allows organizations to '
+							+ 'display their adherence to these requirements. The result is that open source '
+							+ 'license compliance becomes more predictable, ' 
+							+ 'understandable and efficient for participants of the software supply chain.',
+							'This document contains a series of questions to determine whether a company is '
+							+ 'OpenChain Conformant. If each of these questions can be answered with a “yes” '
+							+ 'then that company meets all the requirements of conformance to the OpenChain '
+							+ 'Specification version 1.2. If any of the questions are answered with a “no” '
+							+ 'then the company can clearly identify where additional investment is needed to '
+							+ 'improve the compliance process.'];
 
 /**
  * Formats a question into a string
@@ -68,6 +83,17 @@ function printSection(doc, section) {
 	}
 }
 
+function printPreamble(doc) {
+	doc.fontSize(sectionFontSize);
+	doc.text('Context');
+	doc.moveDown();
+	doc.fontSize(questionFontSize);
+	
+	for (var line in preambleText) {
+		doc.text(preambleText[line]);
+		doc.moveDown();
+	}
+}
 
 /**
  * Creates a PDF file
@@ -95,6 +121,8 @@ function createPdf(inputJsonFileName, outputPdfFileName) {
 	}
 	
 	doc.font(font);	
+	printPreamble(doc);
+	doc.addPage();
 	printSection(doc, questionnaire.sections[0]);
 	for (var i = 1; i < questionnaire.sections.length; i++) {
 		doc.addPage();
